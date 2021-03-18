c2049c99000135a071c1cf08171febeb45f79730
rahou0 <kivenzero@gmail.com>
2021-03-18 16:50:19 +0100

    update

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 //config
 dotenv.config();
 port = process.env.PORT || 5000;
