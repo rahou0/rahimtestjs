629399b1e61729f2ba22d5b4a96789ab32a06519
rahou0 <kivenzero@gmail.com>
2021-03-18 16:43:21 +0100

    update

--- server.js
+++ server.js
@@ -24,5 +24,39 @@ app.get("/post", (req, res) => {
 app.get("/", (req, res) => {
   return res.status(200).send({ titile: "home page" });
 });
-
+app.get("/tranding", (req, res) => {
+  const results = [
+    {
+      id: 1,
+      title: "Djemila",
+      address: "Setif, Algeria",
+    },
+    {
+      id: 2,
+      title: "Prince Abdel Kader Mosque",
+      address: "Constantine, Algeria",
+    },
+    {
+      id: 3,
+      title: "Fort Santa Cruz",
+      address: "Oran, Algeria",
+    },
+    {
+      id: 4,
+      title: "Kasbah of Algiers",
+      address: "Algiers, Algeria",
+    },
+    {
+      id: 5,
+      title: "Memorial du Martyr",
+      address: "Algiers, Algeria",
+    },
+    {
+      id: 6,
+      title: "Timgad",
+      address: "Batna, Algeria",
+    },
+  ];
+  return res.status(200).send(results);
+});
 app.listen(port, () => console.log("server is runing...."));
