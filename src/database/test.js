a4c88151aa8214ba3c015cbb4cdc6233789a0253
rahou0 <kivenzero@gmail.com>
2021-03-20 17:23:41 +0100

    update

--- src/database/test.js
+++ src/database/test.js
@@ -2,6 +2,7 @@ const { connection } = require("../config/database");
 module.exports.getAllUsers = async () => {
   connection.query("SELECT * FROM `Users`", function (err, results) {
     if (err) throw new Error(err);
+    console.log(results);
     return results;
   });
 };
