ae4357411b6bb33a1c9eb757b1ce70bfc431b6a5
rahou0 <kivenzero@gmail.com>
2021-03-20 17:22:25 +0100

    update

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -3,7 +3,7 @@ module.exports.getUsers = async (req, res) => {
   try {
     const results = await getAllUsers();
     if (results) return res.status(400).send("no user in table users");
-    res.status(201).send("you are successfully registered");
+    res.status(201).send(results);
   } catch (error) {
     res.status(500).send(error);
   }
